b88e1fe0035b87bd660240fea8aa77fbd6ff4003
Julien Leforestier <leforestier.julien@gmail.com>
2021-09-03 16:54:01 +0200

    .gitignore

--- contact.php
+++ contact.php
@@ -7,7 +7,9 @@ require_once('inc/header.php');
 // corps de la page
 ?>
 
-<div class="col-md-10 offset-1"></div>
+<div class="col-md-10 offset-1">
+    <a href="https://www.facebook.com/profile.php?id=100063601297728" id="fb" target="_blank">&nbsp;f&nbsp;&nbsp;</a>
+</div>
 
 <?php
 require_once('inc/footer.php');
